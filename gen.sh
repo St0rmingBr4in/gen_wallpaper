57a89865ebf1f43c37d48b32bfb631be2baa53e3
Julien DOCHE <julien.doche@live.fr>
2018-10-21 19:59:38 +0200

    generation: Speed things up and cleaner backgroud color fetching
    Signed-off-by: Julien DOCHE <julien.doche@live.fr>

--- gen.sh
+++ gen.sh
@@ -16,9 +16,8 @@ for img_path in $INPUTDIR/*; do
   for name in $(basename $img_path); do
     echo "Processing $name"
 
-    #background=$(convert $img_path -colorspace rgb -format "%[pixel:p{10,10}]" info:)
-    background=$(convert $img_path -crop 1x1+40+30 -depth 8 -format "%[pixel:p{1,1}]" info:)
+    background=$(convert $img_path -format "%[pixel:p{1,1}]" info:)
 
-    convert $img_path -background $background -resize $targetres -gravity center -extent $targetres $OUTPUTDIR/$name
+    convert $img_path -background $background -resize $targetres -gravity center -extent $targetres $OUTPUTDIR/$name &
   done
 done
