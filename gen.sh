31ddca6e3c16417eceeae0a6908ea6708ac3cf50
Julien DOCHE <julien.doche@gmail.com>
2019-06-09 14:19:19 +0100

    generation: Fix shellcheck issues
    Signed-off-by: Julien DOCHE <julien.doche@gmail.com>

--- gen.sh
+++ gen.sh
@@ -30,26 +30,30 @@ case $scalingmode in
   docker build waifu2x -t waifu2x
 
   echo "Running docker for upscaling"
-  nvidia-docker run -v `pwd`/images:/images waifu2x /images/upscale.sh $OUTPUTW $OUTPUTH /$TOSCALEDIR /$SCALEDDIR
+  nvidia-docker run -v "$(pwd)/images:/images" waifu2x /images/upscale.sh $OUTPUTW $OUTPUTH /$TOSCALEDIR /$SCALEDDIR
   ;;
 esac
 
 [ ! -d $SCALEDDIR ] && echo "Error in scaling" && exit 1
 
-for img_path in $SCALEDDIR/*; do
-  for name in $(basename $img_path)$extention; do
+for img_path in "$SCALEDDIR"/*; do
+  for name in $(basename "$img_path")$extention; do
     echo "Processing $name"
 
+    extended="$TMP/extended_$name"
+    extended_cutted="$TMP/extended_cutted_$name"
+    extended_fill="$TMP/extended_fill_$name"
+
     echo "Generating transparent extended image"
-    convert $img_path -background transparent -resize "$targetres>" -gravity center -extent $targetres $TMP/extended_$name
+    convert "$img_path" -background transparent -resize "$targetres>" -gravity center -extent $targetres "$extended"
 
     echo "Generating transparent extended cutted image"
-    convert $TMP/extended_$name \( -size $(($(identify -format "%w" $img_path) - 2))x$(($(identify -format "%h" $img_path) - 2)) xc:none \) -alpha set -gravity center -compose copy -composite $TMP/extended_cutted_$name
+    convert "$extended" \( -size $(($(identify -format "%w" "$img_path") - 2))x$(($(identify -format "%h" "$img_path") - 2)) xc:none \) -alpha set -gravity center -compose copy -composite "$extended_cutted"
 
     echo "Generating background image"
-    convert $TMP/extended_cutted_$name txt:- | grep -v ImageMagick | grep -v 'graya(0,0)' | grep -v none | sed -e 's/: ([0-9,]\+)  #[0-9A-Z]\+  /,/' |  convert $TMP/extended_cutted_$name -alpha off -sparse-color shepards '@-' $TMP/extended_fill_$name
+    convert "$extended_cutted" txt:- | grep -v ImageMagick | grep -v 'graya(0,0)' | grep -v none | sed -e 's/: ([0-9,]\+)  #[0-9A-Z]\+  /,/' |  convert "$extended_cutted" -alpha off -sparse-color shepards '@-' "$extended_fill"
 
     echo "Generating final image"
-    convert $TMP/extended_fill_$name $TMP/extended_$name -composite $OUTPUTDIR/$name
+    convert "$extended_fill" "$extended" -composite "$OUTPUTDIR/$name"
   done
 done
--- images/upscale.sh
+++ images/upscale.sh
@@ -12,42 +12,42 @@ SCALE_FACTOR=2
 NOISE_CANCEL=2
 EXTRAOPTIONS="-force_cudnn 1 -resume 1"
 
-[ ! -d $INPUTDIR ] && echo "No images to scale" && exit 1
-mkdir -p $OUTPUTDIR
+[ ! -d "$INPUTDIR" ] && echo "No images to scale" && exit 1
+mkdir -p "$OUTPUTDIR"
 
 image_large_enough()
 {
-  imgw=$(identify -format "%w" $1)
-  imgh=$(identify -format "%h" $1)
+  imgw=$(identify -format "%w" "$1")
+  imgh=$(identify -format "%h" "$1")
 
-  [ $imgw -ge $outputw ] || [ $imgh -ge $outputh ]
+  [ "$imgw" -ge "$outputw" ] || [ "$imgh" -ge "$outputh" ]
 }
 
 scale_batch()
 {
-  th waifu2x.lua $EXTRAOPTIONS -noise_level $3 -m noise_scale -scale $SCALE_FACTOR -l $1 -o $2/%s_x${SCALE_FACTOR}.png
+  th waifu2x.lua "$EXTRAOPTIONS" -noise_level "$3" -m noise_scale -scale $SCALE_FACTOR -l "$1" -o "$2/%s_x${SCALE_FACTOR}.png"
 }
 
-while [ -n "$(ls -A $INPUTDIR)" ]; do
+while [ -n "$(ls -A "$INPUTDIR")" ]; do
 
-  for img in $INPUTDIR/*; do
-    if image_large_enough $img; then
-      name=$(basename $img)
+  for img in "$INPUTDIR"/*; do
+    if image_large_enough "$img"; then
+      name=$(basename "$img")
       echo "$name is large enough... skipping"
-      mv $img $OUTPUTDIR/$name
+      mv "$img" "$OUTPUTDIR/$name"
     else
-      echo $img >> to_process.txt
+      echo "$img" >> to_process.txt
     fi
   done
 
   if [ -s to_process.txt ]; then
     echo "Upscaling images:"
     cat to_process.txt
-    scale_batch to_process.txt $INPUTDIR $NOISE_CANCEL
+    scale_batch to_process.txt "$INPUTDIR" $NOISE_CANCEL
   fi
 
-  while read old; do
-    rm $old
+  while read -r old; do
+    rm "$old"
   done < to_process.txt
 
   :> to_process.txt
